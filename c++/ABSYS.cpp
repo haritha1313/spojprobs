50ceb52748e0a547f092c7d1959a313543bccf7c
Noor Alrabea <eleanor.alrabea@gmail.com>
2017-10-03 18:01:07 +0200

    Restructure code. Delete unused includes. Add comments

--- c++/ABSYS.cpp
+++ c++/ABSYS.cpp
@@ -1,55 +1,76 @@
 #include <iostream>
-#include<stdlib.h>
-#include<string.h>
-using namespace std;
+
 int main() {
-	int t;
-	int a1,a2,a3,i;
-	string str1,str2,str3;
-	char ch,ch2;
-	cin>>t;
-	while(t--)
-	{
-		int p=0;
-		cin>>str1>>ch>>str2>>ch2>>str3;
-		i=0;
-		while(str1[i]!='\0')
-		{
-			if(str1[i++]=='m')
-			{p=1;
-			break;}
-		}
-			if(p!=1)
-			a1=stoi(str1);
-		i=0;
-		while(str2[i]!='\0')
-		{if(str2[i++]=='m')
-		{p=2;
-		break;}
- 
-		}
-		if(p!=2)
-			a2=stoi(str2);
-		i=0;
-		while(str3[i]!='\0')
-		{
-			if(str3[i++]=='m')
-			{p=3;
-			break;}
-		}
-		if(p!=3)
-			a3=stoi(str3);
-		switch(p)
-		{
-			case 1:cout<<a3-a2<<" + "<<a2<<" = "<<a3<<"\n";
-					break;
-			case 2:cout<<a1<<" + "<<a3-a1<<" = "<<a3<<"\n";
+	int Eq_amount = 0;
+	int a1 = 0, a2 = 0, a3 = 0;
+	int i = 0;
+	std::string str1, str2, str3;
+	char ch, ch2;
+
+	//Amount of equations to be solved
+	std::cout << "Please enter the amount of equations to be solved" << std::endl;
+	std::cin >> Eq_amount;
+
+	//Solving Equation
+	while(Eq_amount--) {
+		int method = 0;
+
+		std::cout << "insert Equation members include spacebar between members" << std::endl;
+		std::cin >> str1 >> ch >> str2 >> ch2 >> str3;
+
+		//check str1 for "m"
+		while(str1[i] != '\0') {
+			if(str1[i++] == 'm') {
+				method = 1;
 				break;
-			case 3:cout<<a1<<" + "<<a2<<" = "<<a1+a2<<"\n";
-				break;
- 
+			}
 		}
- 
-	}// your code goes here
+
+		//Whether Method 1 should be used
+		if(method != 1) {
+			a1 = stoi(str1);
+			i = 0;
+		}
+
+		//check str2 for "m"
+		while(str2[i] != '\0') {
+			if(str2[i++] == 'm') {
+				method = 2;
+				break;
+			}
+		}
+
+		//Whether method 2 should be used
+		if(method != 2) {
+			a2 = stoi(str2);
+			i = 0;
+		}
+
+		//check str3 for "m"
+		while(str3[i] != '\0') {
+			if(str3[i++] == 'm') {
+				method = 3;
+				break;
+			}
+		}
+
+		//Whether method 3 should be used
+		if(method != 3) {
+			a3 = stoi(str3);
+			i = 0;
+		}
+
+		//Print out result
+		switch(method)
+		{
+			case 1: 	std::cout << a3-a2 <<" + "<< a2 <<" = "<< a3 <<"\n";
+				break;
+			case 2: 	std::cout << a1 <<" + "<< a3-a1 <<" = "<< a3 <<"\n";
+				break;
+			case 3:		std::cout << a1 <<" + "<< a2 <<" = "<< a1+a2 <<"\n";
+				break;
+		}
+	}
+
 	return 0;
 }
