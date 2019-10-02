0ceee2aac643f3493433899063a95c3fbe58b79a
Manish Kumar <34408076+manishprivet@users.noreply.github.com>
2019-10-02 22:16:13 +0530

    Added comments manishprivet

--- c/ADDREV.c
+++ c/ADDREV.c
@@ -1,13 +1,14 @@
+//Importing Header
 #include <stdio.h>
 
-long numrev(long int x)
+long numrev(long int x) //Function to reverse a number by Digit Extraction
 {
 		int rem, rev=0;
 		while(x > 0)
 		{
-			rem = x % 10;
+			rem = x % 10; //Last digit is extracted
 			rev = rev * 10 + rem;
-			x /= 10;
+			x /= 10; //x is reduced by one digit
 		}
 		return rev;
 }
@@ -17,14 +18,14 @@ int main()
 	int N;
 	long int a, b, i;
 	
-	scanf("%d", &N);
-	for(i = 0; i < N; i++)
+	scanf("%d", &N); //Number of test cases is entered
+	for(i = 0; i < N; i++) //Loop from 1 to N is run
 	{
-		scanf("%ld %ld", &a, &b);
-		a = numrev(a);
-		b = numrev(b);
-		a += b;
-		printf("%ld\n", numrev(a));
+		scanf("%ld %ld", &a, &b); //two integers are taken
+		a = numrev(a); //a is reversed
+		b = numrev(b); //b is reversed
+		a += b; //a and b are added and it's stored in a
+		printf("%ld\n", numrev(a)); //reverse of sum of reverse of a and b is printed
 	}
 
 	return 0;
